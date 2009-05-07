b5932f9942abc9793f44f74b235fc57b229619d3
Jared Grippe <jared@deadlyicon.com>
2009-05-07 11:30:21 -0700

    ignoring doc and fixing some docs in hash

--- lib/proctologist/hash.rb
+++ lib/proctologist/hash.rb
@@ -1,15 +1,15 @@
 class Hash
-  #  Converts the key value pairs into method argument pairs and calls them
-  #  unorderly on the objects in the enumberable
+  # Converts the key value pairs into method argument pairs and calls them
+  # unorderly on the objects in the enumberable
   #
-  #  module StringExtension
-  #    def awesomize!
-  #      "#{self.upcase} IS AWESOME!"
-  #    end
-  #  end
-  #
-  #  ['jared','aaron'].map(&{:extend => StringExtension}).map(&{:awesomize! => nil})
-  #  => ["JARED IS AWESOME!", "AARON IS AWESOME!"]
+  #   module StringExtension
+  #     def awesomize!
+  #       "#{self.upcase} IS AWESOME!"
+  #     end
+  #   end
+  #   
+  #   ['jared','aaron'].map(&{:extend => StringExtension}).map(&{:awesomize! => nil})
+  #   => ["JARED IS AWESOME!", "AARON IS AWESOME!"]
   def to_proc
     ::Proc.new do |target|
       self.inject(target) do |target, options, ass|
